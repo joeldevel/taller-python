e5d9ee479991fac3584324eeb8354a4d26134d14
joeldevel <christianjoel.developer@gmail.com>
2021-11-02 07:56:25 -0300

    Ejemplo de variable con tipado dinámico

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Variables
+# Las cosas tienen tipo
+# No hay que declarar el tipo
+# El tipo puede cambiar durante la vida del programa
+
+variable_1 = "moringa"
+
+print(variable_1 ," es ",type(variable_1)) # Nos da la clase str
+
+variable_1 = 123
+
+print(variable_1, " es ",type(variable_1)) # Nos da la clase int
+
+variable_1 = 123.0
+
+print(variable_1, " es ", type(variable_1)) # Nos da la clase float
+
+# Y asi
